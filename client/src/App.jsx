eb45e223544db42f004e5ba4be8713f91460e2cf
Abhinav <abhinavajay20@gmail.com>
2025-03-30 17:52:23 +0530

    setting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element= {<About/>}/>
+          <Route path = '/signup' element = {<Signup/>}/>
+          <Route path='/signin' element={<Signin/>}/>
+          <Route path = '/dashboard' element = {<Dashboard/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500'>This is the heading</h1>
+      
+    </div>
+  )
+}
