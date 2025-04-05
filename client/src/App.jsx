4d92ddffbfc24dfc8cc60c8b2dd09b228567be75
Abhinav <abhinavajay20@gmail.com>
2025-04-06 00:11:19 +0530

    creating header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,18 @@ import About from "./pages/About";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import Projects from './pages/Projects';
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element= {<About/>}/>
           <Route path = '/signup' element = {<Signup/>}/>
           <Route path='/signin' element={<Signin/>}/>
+          <Route path='/projects' element = {<Projects/>}/>
           <Route path = '/dashboard' element = {<Dashboard/>}/>
         </Routes>
     </BrowserRouter>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl text-red-500'>This is the heading</h1>
+      <h1 className='text-3xl text-red-500'>Home</h1>
       
     </div>
   )
