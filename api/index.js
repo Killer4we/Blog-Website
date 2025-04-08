217d3b5f99fd57fe8bf08ca1ece5dea5f980cf34
Abhinav <abhinavajay20@gmail.com>
2025-04-08 22:38:04 +0530

    connecting mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+
+mongoose.connect("mongodb+srv://abhinavajay20:TimVfaJbAbYtJKdU@cluster0.k65pq7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    console.log("MongoDb Connected");
+}).catch(err=>{
+    console.log("Error connecting MongoDb");
+});
+
 
 app.listen(300,()=>{
     console.log("Server is running on Port 3000");
