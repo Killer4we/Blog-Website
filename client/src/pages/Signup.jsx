c9fdc6633ecbc58444eabbc076d5d71fe8af16a9
Abhinav <abhinavajay20@gmail.com>
2025-04-25 00:34:50 +0530

    Completed the functionality of the sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, FloatingLabel, Label, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { Alert, Button, FloatingLabel, Label, TextInput } from 'flowbite-react'
 
 export default function Signup() {
+  const [formData,setFormData] = useState({});
+  const [errorMessage,setErrorMessage] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()}); /* making sure whitespaces are removed */
+  };
+  // console.log(formData);
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    if(!formData.username || !formData.password || !formData.email){
+      return setErrorMessage('Please fill out all the fields');
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup',{  /* Now i need to set a proxy server too so that api calls can be made to the correct port number */
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success===false){
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/signin');
+      }
+    }catch(error){
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -18,25 +52,34 @@ export default function Signup() {
         </div>
         {/* right side */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value = 'Your UserName'/>
-              <TextInput type='text' placeholder='Username' id='username'/>
+              <TextInput type='text' placeholder='Username' id='username' onChange={handleChange}/>
             </div>
             <div>
               <Label value = 'Your Email'/>
-              <TextInput type='email' placeholder='Email' id='email'/>
+              <TextInput type='email' placeholder='Email' id='email' onChange={handleChange}/>
             </div>
             <div>
               <Label value = 'Your Password'/>
-              <TextInput type='password' placeholder='Password' id='password'/>
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
             </div>
-            <Button type='submit'>Sign Up</Button>
+            <Button type='submit' disabled={loading}>
+              {
+                loading?'Loading':'Sign Up'
+              }
+            </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'> {errorMessage}</Alert>
+            )
+          }
         </div>
       </div>
     </div>
